a9c652d7dac46938054b0aaea3d967ddc7bd69db
Sungmin Park <Neamhain@Sungmins-MacBook-Pro.local>
2017-10-26 16:48:23 -0400

    limit client movement update

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -66,15 +66,15 @@ var updateMovement = function updateMovement() {
   user.alpha = 0.05;
 
   // if this client's user moves or is falling from gravity - send to server to update server
-  // if (updated === true || !user.grounded) {
-  socket.emit('updatePlayer', {
-    pos: user.pos,
-    prevPos: user.prevPos,
-    destPos: user.destPos,
-    alpha: user.alpha,
-    jump: jumped
-  });
-  // }
+  if (updated === true || !user.grounded) {
+    socket.emit('updatePlayer', {
+      pos: user.pos,
+      prevPos: user.prevPos,
+      destPos: user.destPos,
+      alpha: user.alpha,
+      jump: jumped
+    });
+  }
 };
 
 // draw players
